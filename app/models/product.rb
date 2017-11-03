68715a16849bdc92911911a5837fe7b7111cb638
kunruh9 <kyleunruh@u.boisestate.edu>
2017-11-03 17:04:22 -0600

    This magic is hurting my brain

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
 
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_items
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
@@ -9,4 +13,13 @@ class Product < ApplicationRecord
   }
   validates :title, length: { minimum: 10, message: 'title is too short, must be minimum of 10 characters' }
 
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_items
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
