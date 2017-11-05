d21471a7551d30b6b2ad5ab85b718c7e4990fe69
kunruh9 <kyleunruh@u.boisestate.edu>
2017-11-05 10:19:06 -0700

    Finish Task E playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
+
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+
+    session[:counter] += 1
+
     @products = Product.order(:title)
+
+    puts "Store accessed #{session[:counter]} times\n"
   end
 end
