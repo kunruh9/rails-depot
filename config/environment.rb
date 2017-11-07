dfefecd5c3b6bc4a112af4bbcddefa68a661537a
kunruh9 <kyleunruh@u.boisestate.edu>
2017-11-07 13:52:05 -0800

    Stops mid Task H1

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    authentication:       "plain",
+    user_name:            "kunruh9@gmail.com",
+    password:             "d0YUERkUQ7l7mM7^rDe&1",
+    enable_starttls_auto: true
+  }
+end
